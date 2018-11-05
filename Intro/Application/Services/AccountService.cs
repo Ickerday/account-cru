211a74594c30013529364f5c063e2513b2c2cc4d
Bartosz <elitarform@gmail.com>
2018-11-05 17:24:49 +0100

    Add AccountService

---
+++ Intro/Application/Services/AccountService.cs
@@ -0,0 +1,39 @@
+﻿using Intro.Core.Entities;
+using Intro.Persistence;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Intro.Application.Services
+{
+    public interface IAccountService
+    {
+        void Add(Account account);
+        IEnumerable<Account> Find(ulong number);
+        void Update(Account account);
+    }
+
+    public class AccountService : IAccountService
+    {
+        private readonly AccountingContext _context;
+
+        public AccountService(AccountingContext context) => 
+            _context = context;
+
+        public void Add(Account account)
+        {
+            _context.Accounts
+                .Add(account);
+
+            _context.SaveChanges();
+        }
+
+        public void Update(Account account)
+        {
+            _context.Update(account);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Account> Find(ulong number) => _context.Accounts
+                .Where(x => x.Number.Equals(number));
+    }
+}
