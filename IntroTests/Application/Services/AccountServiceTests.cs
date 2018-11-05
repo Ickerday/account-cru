9ef74d4cb0ffda87508748bc8e0a81b88a272f9a
Bartosz <elitarform@gmail.com>
2018-11-05 17:25:20 +0100

    Setup AccountServiceTests boilerplate

---
+++ IntroTests/Application/Services/AccountServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Intro.Application.Services;
+using Intro.Persistence;
+using Moq;
+using Xunit;
+
+namespace IntroTests
+{
+    public class AccountServiceTests
+    {
+        private readonly AccountingContext _context;
+        private readonly AccountService _service;
+
+        public AccountServiceTests()
+        {
+            _context = Mock.Of<AccountingContext>();
+            _service = new AccountService(_context);
+        }
+
+        [Fact(Skip ="Unwritten")]
+        public void Service__UsesAccountingContext()
+        {
+
+        }
+
+        [Fact(Skip = "Unwritten")]
+        public void Service__AddsNewAccount()
+        {
+
+        }
+
+        [Fact(Skip = "Unwritten")]
+        public void Service__Should()
+        {
+
+        }
+    }
+}
