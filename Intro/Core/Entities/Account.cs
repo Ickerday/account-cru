ef9d8ba2b4726b0109ed1c4a7516c68c7dc1f830
Bartosz <elitarform@gmail.com>
2018-11-05 13:35:28 +0100

    Add Account model

---
+++ Intro/Core/Entities/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace Intro.Core.Entities
+{
+    public class Account
+    {
+        public ulong Number { get; set; }
+        public string Name { get; set; }
+        public decimal AvailableFunds { get; set; }
+        public decimal Balance { get; set; }
+        public bool HasCard { get; set; }
+    }
+}
