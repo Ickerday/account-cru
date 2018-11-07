b1b02ec1c492de9e3b6251a740e5348e1cbc56ef
Bartosz <elitarform@gmail.com>
2018-11-07 14:59:33 +0100

    Add logging to AccountsController

--- Intro/Controllers/AccountsController.cs
+++ Intro/Controllers/AccountsController.cs
@@ -2,6 +2,7 @@
 using Intro.Application.Services;
 using Intro.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,33 +13,66 @@ namespace Intro.Controllers
     [ApiController]
     public class AccountsController : ControllerBase
     {
+        private readonly ILogger<AccountsController> _logger;
         private readonly IAccountService _accountService;
 
-        public AccountsController(IAccountService accountService)
+        public AccountsController(IAccountService accountService, ILogger<AccountsController> logger)
         {
+            _logger = logger;
             _accountService = accountService;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Account>> Get() => _accountService.GetAccounts()
-                .ToArray();
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            try
+            {
+                return _accountService.GetAccounts()
+                    .ToArray();
+            }
+            catch
+            {
+                _logger.LogWarning("Couldn't get Accounts");
+                return StatusCode(503);
+            }
+        }
 
         [HttpGet("{id}")]
         public ActionResult<Account> Get(ulong id)
         {
-            var result = _accountService.Find(id);
+            try
+            {
+                var result = _accountService.Find(id);
 
-            if (result != null)
-                return result;
+                if (result != null)
+                {
+                    _logger.LogInformation($"Found Account with ID {id}");
+                    return result;
+                }
 
-            return NotFound();
+                _logger.LogWarning($"Couldn't find Account with ID {id}");
+                return NotFound();
+            }
+            catch
+            {
+                _logger.LogWarning($"Invalid request for Account with ID {id}");
+                return BadRequest();
+            }
         }
 
         [HttpPost]
         public ActionResult<Account> Add(Account account)
         {
-            _accountService.Add(account);
-            return CreatedAtAction("Add", new { id = account.Id }, account);
+            try
+            {
+                _accountService.Add(account);
+                _logger.LogInformation($"Created Account with ID {account.Id}");
+                return CreatedAtAction("Add", new { id = account.Id }, account);
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
 
         [HttpPut("{id}")]
@@ -47,13 +81,16 @@ namespace Intro.Controllers
             try
             {
                 _accountService.Update(id, account);
+                _logger.LogInformation($"Updated Account with ID {id}");
             }
             catch (AccountNotFoundException)
             {
+                _logger.LogWarning($"Couldn't update Account with ID {id}");
                 return BadRequest();
             }
+
+            // log here
             return NoContent();
         }
-
     }
 }
--- IntroTests/Controllers/AccountsControllerTests.cs
+++ IntroTests/Controllers/AccountsControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Intro.Application.Services;
 using Intro.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
 using static IntroTests.AccountHelpers;
@@ -19,8 +20,11 @@ namespace IntroTests.Controllers
             // Arrange
             var account = GetMockAccount(id, name, availableFunds, balance, hasCard);
 
+            var mockLogger = new Mock<ILogger<AccountsController>>();
             var mockService = new Mock<IAccountService>();
-            _controller = new AccountsController(mockService.Object);
+
+            _controller = new AccountsController(mockService.Object, mockLogger.Object);
+
             // Act
             var response = _controller.Add(account);
 
@@ -32,28 +36,29 @@ namespace IntroTests.Controllers
         public void Controller__UpdatesAccounts()
         {
             // Arrange
-            var id = 49203841098409218UL;
-            var hasCard = true;
+            const ulong id = 49203841098409218UL;
+            const bool hasCard = true;
 
-            var oldName = "test_value1";
-            var oldFunds = 123;
-            var oldBalance = 456;
+            const string oldName = "test_value1";
+            const int oldFunds = 123;
+            const int oldBalance = 456;
 
-            var newName = "test_value2";
-            var newFunds = 123123;
-            var newBalance = 456456;
+            const string newName = "test_value2";
+            const int newFunds = 123123;
+            const int newBalance = 456456;
 
             var oldAccount = GetMockAccount(id, oldName, oldFunds, oldBalance, hasCard);
             var newAccount = GetMockAccount(id, newName, newFunds, newBalance, hasCard);
 
+            var mockLogger = new Mock<ILogger<AccountsController>>();
             var mockService = new Mock<IAccountService>();
             mockService.Setup(x => x.Find(id))
                 .Returns(newAccount);
 
-            _controller = new AccountsController(mockService.Object);
+            _controller = new AccountsController(mockService.Object, mockLogger.Object);
 
             // Act
-            var addResponse = _controller.Add(oldAccount);
+            _controller.Add(oldAccount);
             var updateResponse = _controller.Update(id, newAccount);
             var updatedAccount = _controller.Get(id);
 
@@ -69,11 +74,13 @@ namespace IntroTests.Controllers
                 // Arrange
                 var accountList = new[] { GetMockAccount(0, "test1", 123, 456, false) };
 
+                var mockLogger = new Mock<ILogger<AccountsController>>();
                 var mockService = new Mock<IAccountService>();
                 mockService.Setup(x => x.GetAccounts())
                     .Returns(accountList);
 
-                _controller = new AccountsController(mockService.Object);
+                _controller = new AccountsController(mockService.Object, mockLogger.Object);
+
                 // Act
                 var response = _controller.Get();
 
