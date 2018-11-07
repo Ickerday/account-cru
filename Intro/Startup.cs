38ae5546b45b4e702a961448e25a1a1d0187c26a
Bartosz <elitarform@gmail.com>
2018-11-07 15:00:25 +0100

    Add MongoDB driver

--- Intro/Startup.cs
+++ Intro/Startup.cs
@@ -1,4 +1,4 @@
-﻿using Intro.Application.Services;
+using Intro.Application.Services;
 using Intro.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +28,6 @@ namespace Intro
             services.AddTransient<IAccountService, AccountService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
