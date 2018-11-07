7d25b2fd2f3c135d95e76957b272c7158c5c0958
Bartosz <elitarform@gmail.com>
2018-11-07 14:57:46 +0100

    Set up logging with NLog

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -1,17 +1,45 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+using System;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace Intro
 {
     public class Program
     {
+        private static readonly Logger Logger = NLogBuilder.ConfigureNLog("nlog.config")
+            .GetCurrentClassLogger();
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            try
+            {
+                Logger.Debug("Starting Main()");
+                CreateWebHostBuilder(args).Build()
+                    .Run();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
