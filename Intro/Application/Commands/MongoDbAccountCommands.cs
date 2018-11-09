d3267505d9ef061f688d91958f6cfdc3087f2fb1
Bartosz <elitarform@gmail.com>
2018-11-09 13:47:09 +0100

    Add MongoDbAccountCommands

---
+++ Intro/Application/Commands/MongoDbAccountCommands.cs
@@ -0,0 +1,33 @@
+﻿using Intro.Core.Entities;
+using Intro.Persistence.MongoDb;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+
+namespace Intro.Application.Commands
+{
+    public class MongoDbAccountCommands : IAccountCommands
+    {
+        private readonly MongoDbContext _context;
+        private readonly ILogger<MongoDbAccountCommands> _logger;
+
+        public MongoDbAccountCommands(MongoDbContext context, ILogger<MongoDbAccountCommands> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public void Update(Account account)
+        {
+            _logger.LogInformation($"Updating account with ID {account.Id} in {_context.GetType().FullName}");
+            _context.Accounts
+                .ReplaceOne(x => x.Id == account.Id, account);
+        }
+
+        public void Add(Account account)
+        {
+            _logger.LogInformation($"Adding account with ID {account.Id} to {_context.GetType().FullName}");
+            _context.Accounts
+                .InsertOne(account);
+        }
+    }
+}
