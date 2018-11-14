84b8e04de738c06ad1e407bfbf8f698c50e3e047
Bartosz <elitarform@gmail.com>
2018-11-14 11:50:13 +0100

    Try implementing AndSpecification

---
+++ AccountService.Core/Search/AndSpecification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace AccountService.Core.Search
+{
+    public class AndSpecification<T> : Specification<T>
+    {
+        private readonly ISpecification<T> _left;
+        private readonly ISpecification<T> _right;
+
+        public AndSpecification(ISpecification<T> left, ISpecification<T> right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var leftExpression = _left.ToExpression();
+            var rightExpression = _right.ToExpression();
+
+            var andExpression = Expression.AndAlso(leftExpression.Body, rightExpression.Body);
+            return Expression.Lambda<Func<T, bool>>(andExpression, leftExpression.Parameters);
+        }
+    }
+}
