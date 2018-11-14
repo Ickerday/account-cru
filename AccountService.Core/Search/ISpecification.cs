a8cf5235273cb1f4ef310bce6103057fe5599a26
Bartosz <elitarform@gmail.com>
2018-11-14 11:41:21 +0100

    Add abstract Specification and ISpecification

--- AccountService.Core/Search/ISpecification.cs
+++ AccountService.Core/Search/ISpecification.cs
@@ -1,8 +1,12 @@
-﻿namespace AccountService.Application.Search
+﻿using System;
+using System.Linq.Expressions;
+
+namespace AccountService.Core.Search
 {
     public interface ISpecification<T>
     {
-        bool IsSatisfiedBy(T o);
+        bool IsSatisfiedBy(T entity);
+        Expression<Func<T, bool>> ToExpression();
         ISpecification<T> And(ISpecification<T> specification);
         ISpecification<T> Or(ISpecification<T> specification);
         ISpecification<T> Not(ISpecification<T> specification);
---
+++ AccountService.Core/Search/Specification.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+using AccountService.Core.Exceptions.Specification;
+
+namespace AccountService.Core.Search
+{
+    public abstract class Specification<T> : ISpecification<T>
+    {
+        public bool IsSatisfiedBy(T entity)
+        {
+            if (entity == null)
+                throw new InvalidSpecificationException();
+            return ToExpression().Compile()
+                .Invoke(entity);
+        }
+
+        public abstract Expression<Func<T, bool>> ToExpression();
+
+        public ISpecification<T> And(ISpecification<T> specification)
+        {
+            return new AndSpecification<T>(this, specification);
+        }
+
+        public ISpecification<T> Or(ISpecification<T> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISpecification<T> Not(ISpecification<T> specification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+}
