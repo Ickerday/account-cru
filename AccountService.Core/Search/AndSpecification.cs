b153856e8dee23e65ad1140afb7e6a07c6872ade
Bartosz <elitarform@gmail.com>
2018-11-14 14:52:48 +0100

    Begin implementing And/Or/Not Specifications

--- AccountService.Core/Search/AndSpecification.cs
+++ AccountService.Core/Search/AndSpecification.cs
@@ -5,10 +5,10 @@ namespace AccountService.Core.Search
 {
     public class AndSpecification<T> : Specification<T>
     {
-        private readonly ISpecification<T> _left;
-        private readonly ISpecification<T> _right;
+        private readonly Specification<T> _left;
+        private readonly Specification<T> _right;
 
-        public AndSpecification(ISpecification<T> left, ISpecification<T> right)
+        public AndSpecification(Specification<T> left, Specification<T> right)
         {
             _left = left;
             _right = right;
---
+++ AccountService.Core/Search/NotSpecification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace AccountService.Core.Search
+{
+    public class NotSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> _left;
+        private readonly Specification<T> _right;
+
+        public NotSpecification(Specification<T> left, Specification<T> right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
