5d38dee8de449e8f73e3aa82ab36d90d832316d9
Bartosz <elitarform@gmail.com>
2018-11-14 14:51:12 +0100

    Add more Specifications

---
+++ AccountService/Search/AccountAvailableFundsAtLeastSpecification.cs
@@ -0,0 +1,18 @@
+﻿using AccountService.Core.Entities;
+using AccountService.Core.Search;
+using System;
+using System.Linq.Expressions;
+
+namespace AccountService.Search
+{
+    public class AccountAvailableFundsAtLeastSpecification : Specification<Account>
+    {
+        public decimal AvailableFunds { get; }
+
+        public AccountAvailableFundsAtLeastSpecification(decimal availableFunds) =>
+            AvailableFunds = availableFunds;
+
+        public override Expression<Func<Account, bool>> ToExpression() =>
+            x => x.AvailableFunds >= AvailableFunds;
+    }
+}
---
+++ AccountService/Search/AccountAvailableFundsLessThanSpecification.cs
@@ -0,0 +1,18 @@
+﻿using AccountService.Core.Entities;
+using AccountService.Core.Search;
+using System;
+using System.Linq.Expressions;
+
+namespace AccountService.Search
+{
+    public class AccountAvailableFundsLessThanSpecification : Specification<Account>
+    {
+        public decimal AvailableFunds { get; }
+
+        public AccountAvailableFundsLessThanSpecification(decimal availableFunds) =>
+            AvailableFunds = availableFunds;
+
+        public override Expression<Func<Account, bool>> ToExpression() =>
+            x => x.AvailableFunds < AvailableFunds;
+    }
+}
