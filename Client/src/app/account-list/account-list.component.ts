be363d22982be4b99d7e632e755ece9ef2ec8cff
Bartosz <elitarform@gmail.com>
2018-11-22 13:18:59 +0100

    Add AccountListComponent

---
+++ Client/src/app/account-list/account-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AccountsClient } from '../client/client';
+
+@Component({
+  selector: 'app-account-list',
+  templateUrl: './account-list.component.html',
+  styleUrls: ['./account-list.component.scss']
+})
+export class AccountListComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private client: AccountsClient
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
