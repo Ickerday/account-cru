03f169f35da47dee622068ed8d9a8eecfba15c92
Bartosz <elitarform@gmail.com>
2018-11-22 13:19:37 +0100

    Declare components and add routing

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { AccountListComponent } from './account-list/account-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AccountDetailComponent } from './account-detail/account-detail.component';
+
+const appRoutes: Routes = [
+  { path: 'accounts', component: AccountListComponent },
+  { path: 'accounts/:id', component: AccountDetailComponent },
+  { path: '**', component: PageNotFoundComponent },
+  { path: '', redirectTo: '/accounts', pathMatch: 'full' },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountListComponent,
+    AccountDetailComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: !environment.production }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
